43bc676f25bffd7fe16d43db55da97223c8f5764
Josh Arnold <josharnold997@gmail.com>
2019-09-10 15:06:07 -0700

    Initial commit

---
+++ Example/SpringButton/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  SpringButton
+//
+//  Created by josharnoldjosh on 09/10/2019.
+//  Copyright (c) 2019 josharnoldjosh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
